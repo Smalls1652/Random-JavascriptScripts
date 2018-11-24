fe790079841d8d4689e780b512ae80f4517c584e
Timothy Small <smalls.online@icloud.com>
2018-11-24 14:31:10 -0500

    Initial Commit
    This is the template for my code structure.

---
+++ template.js
@@ -0,0 +1,36 @@
+/*
+
+<- ADD SCRIPT TITLE HERE ->
+
+Created by: Smalls
+Website - https://smalls.online
+Github - https://github.com/Smalls1652
+
+Description:
+<- ADD DESCRIPTION OF SCRIPT HERE ->
+
+Notes:
+<- ADD ANY ADDITIONAL NOTES HERE ->
+
+*/
+
+// Begin User Variables -->
+
+/* ADD GlOBAL VARIABLES THAT THE USER CAN CHANGE */
+
+// <-- End User Variables
+
+
+// Do not edit below unless you know what you're doing
+
+// Begin function declarations -->
+
+/* ADD FUNCTIONS HERE  */
+
+// <-- End function declarations
+
+// Begin script actions -->
+
+/* ADD FUNCTIONS TO RUN HERE */
+
+// <-- End script actions.
