fd883146fa32839b3e561c2dea6e7a8f5618249b
Timothy Small <smalls.online@icloud.com>
2019-01-28 23:54:34 -0500

    Updated to 2.0.
    Revamped the script with these changes:
    - Restructured the comments.
    - Added a UITable to return when ran from Siri. (Note: When ran from
    the Shortcuts widget or an Apple Watch, a UITable won't be shown.)
    - Changed the output of the success text to be shorter.
    - Cleaned up the code to include semicolons.

--- Scriptable/Complete-Daily-Reminders.js
+++ Scriptable/Complete-Daily-Reminders.js
@@ -12,106 +12,155 @@ Completes every reminder that's due today in a given calendar.
 The script is built for the iOS app Scriptable and will only work for that app.
 
 Notes:
-Change the variable ReminderList (Located on Line 21) to whatever your desired reminder list is.
+Change the variable ReminderList (Located on Line 19) to whatever your desired reminder list is.
 
 */
 
-// Begin User Variables -->
-
-let ReminderList = "Daily Reminders" //The name of the reminder list
-
-// <-- End User Variables
-
+let ReminderList = "Morning Reminders"; //The name of the reminder list
 
 // Do not edit below unless you know what you're doing
 
-// Begin function declarations -->
-
-function getRemindersCal(list) { //Get the reminder calendar and return it.
+function getRemindersCal(list) {
+	//Get the reminder calendar and return it.
 	
-	return Calendar.forRemindersByTitle(list)
+	return Calendar.forRemindersByTitle(list);
 }
 
-function getRemindersForToday(calendar) { //Get the reminders due today and return them.
+function getRemindersForToday(calendar) {
+	//Get the reminders due today and return them.
 
-	return Reminder.incompleteDueToday([calendar])
+	return 	Reminder.incompleteDueToday([calendar]);
 }
 
-function checkRemindersLength(rlist) { //Checks the reminders list length.
+function checkRemindersLength(rlist) { 
+	//Checks the reminders list length.
 
 	if (rlist.length == 0) {
 		//If Reminders is empty, then break the script and announce it.
-		throw new Error("There are no reminders due today to complete in your list " + ReminderList + ".")
+		throw new Error("There are no reminders due today to complete in your list " + ReminderList + ".");
 	}
 }
 
-function completeReminder(reminder){ //Reminders are set to complete and saved.
+function completeReminder(reminder){ 
+	//Reminders are set to complete and saved.
 
-	reminder.isCompleted = true //Sets the reminder to complete.
-	console.log(reminder.title + " has been marked as completed.")
-	reminder.save() //Saves the completion to the reminder.
-	console.log("Saved!")
+	reminder.isCompleted = true; //Sets the reminder to complete.
+	console.log(reminder.title + " has been marked as completed.");
+	reminder.save(); //Saves the completion to the reminder.
+	console.log("Saved!");
 }
 
-function handleError(returnedError) { //Handles errors thrown it's way.
+function handleError(returnedError) { 
+	//Handles errors thrown it's way.
 
-	if (config.runsWithSiri) { 
-		outputText("Hmm... There seems to be an issue.\n\n" + returnedError.message)
-		Script.complete()
+	if (config.runsWithSiri) {
+		outputText("Hmm... There seems to be an issue.\n\n" + returnedError.message);
+		
+		let ErrorTable = new UITable();
+		ErrorTable.showSeparators = true;
+		
+		let ErrorTableHeader = new UITableRow();
+		ErrorTableHeader.isHeader = true;
+		ErrorTableHeader.addCell(UITableCell.text("There seems to have been an issue."))
+		
+		let ErrorTableInfo = new UITableRow();
+		ErrorTableInfo.height = 120;
+		ErrorTableInfo.addCell(UITableCell.text(returnedError.message))
+		
+		ErrorTable.addRow(ErrorTableHeader);
+		ErrorTable.addRow(ErrorTableInfo);
+		
+		ErrorTable.present();
 	}
 	else if (config.runsInApp) {
-		outputText("There seems to be an issue.\n\n" + returnedError.message)
-		Script.complete()
+		outputText("There seems to be an issue.\n\n" + returnedError.message);
 	}
 	else {
-		throw returnedError
+		throw returnedError;
 	}
 }
 
-function outputText(text) {  //Choose the proper output option
+function outputText(text) {  
+	//Choose the proper output option
+	//Always output text to console for logging purposes.
+	console.log(text);
 	
 	if (config.runsWithSiri) {
-	//If Siri is used to invoke the script.
-		Speech.speak(text)
+		//If Siri is used to invoke the script.
+		Speech.speak(text);
 	}
-	else if (config.runsInApp) {
-	//If it's ran inside of Scriptable.
-		outputAlert = new Alert()
-		outputAlert.title = "Complete Daily Reminders"
-		outputAlert.message = text
-		outputAlert.addAction("Okay")
+	else {
+		//If anything else, present an alert.
+		let ScriptAlert = new Alert();
+		ScriptAlert.title = Script.name();
+		ScriptAlert.message = text;
 		
-		outputAlert.presentAlert()
+		ScriptAlert.presentAlert();
 	}
-	
-	//Always output text to console for logging purposes.
-	console.log(text)
 }
 	
-function completeReminderList() { //Main action function to run the script.
+async function completeReminderList() { 
 
-	getRemindersCal(ReminderList) //Get the calendar for the reminder list.
+	//Main action function to run the script.
+
+	await getRemindersCal(ReminderList) //Get the calendar for the reminder list.
 	.then(ReminderCal => getRemindersForToday(ReminderCal)) //Get the reminders due today in the list.
 	.then(r => {
 		
-		checkRemindersLength(r) //Check the length of the reminders returned
-		return r
+		checkRemindersLength(r); //Check the length of the reminders returned
+		return r;
 	})
-	.then(Reminders => { //Iterate each reminder object and mark as complete. Announce it's completion when done.
-		
-		Reminders.map(rem => completeReminder(rem))
-		
-		outputText("Reminders due today, from your list " + ReminderList + ", have been marked complete.")
-	})
-	.catch(err => handleError(err)) //Any errors are thrown in this catch and reported back through the handleError() function.
+	.then(Reminders => { 
 	
-	Script.complete()
+		//Iterate each reminder object and mark as complete. Announce it's completion when done.
+		
+		Reminders.map(rem => completeReminder(rem));
+		
+		//Build the table to show.
+		let ReminderTable = new UITable();
+		
+		//Table Header
+		let TableHeader = new UITableRow();
+		
+		let TableHeaderTitleCell = UITableCell.text("Title");
+		TableHeaderTitleCell.leftAligned();
+		TableHeaderTitleCell.widthWeight = 50;
+		
+		let TableHeaderCompletedCell = UITableCell.text("Completed");
+		TableHeaderCompletedCell.centerAligned();
+		TableHeaderCompletedCell.widthWeight = 35;
+		
+		TableHeader.addCell(TableHeaderTitleCell);
+		TableHeader.addCell(TableHeaderCompletedCell);
+		TableHeader.isHeader = true;
+		
+		ReminderTable.addRow(TableHeader);
+		
+		//Add each reminder completed to the table
+		Reminders.map(rem => {
+			let row = new UITableRow();
+			
+			let rowTitleCell = UITableCell.text(rem.title);
+			rowTitleCell.leftAligned();
+			rowTitleCell.widthWeight = 50;
+			
+			let rowCompletedCell = UITableCell.text("☑️");
+			rowCompletedCell.centerAligned();
+			rowCompletedCell.widthWeight = 35;
+			
+			
+			row.addCell(rowTitleCell);
+			row.addCell(rowCompletedCell);
+			ReminderTable.addRow(row);
+		});
+		
+		ReminderTable.showSeparators = true;
+		
+		ReminderTable.present();
+		
+		outputText("\nReminders, in " + ReminderList + ", have been marked complete.");
+	})
+	.catch(err => handleError(err)); //Any errors are thrown in this catch and reported back through the handleError() function.
 }
 
-// <-- End function declarations
-
-// Begin script actions -->
-
-await completeReminderList() //The main action that runs the script.
-
-// <-- End script actions.
+await completeReminderList(); //The main action that runs the script.
